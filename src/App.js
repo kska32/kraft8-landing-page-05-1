918f4525e812501c81184eafa67f40afa37a8a4d
kska32 <kska32@gmail.com>
2019-12-18 21:58:36 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Helmet} from "react-helmet";
+
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Helmet>
+        <meta name="description" content="A Simple Test, kraft8-landing-page-05-1"/>
+        <meta name="keywords" content="kraft8-landing-page-05-1"/>
+      </Helmet>
+      Hello
     </div>
   );
 }
---
+++ src/components/01-Header/index.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+import "Header01.scss";
+
+export default class Header01 extends Component{
+    render(){
+        return <div className="Header01">
+            <div className="Header01Wrapper">
+                
+            </div>
+        </div>
+    }
+}
