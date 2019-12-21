c27c6b0789237dcc16d590658b62b4b40fba4f40
kska32 <kska32@gmail.com>
2019-12-21 22:34:00 +0900

    .

--- docs/precache-manifest.2068e194c780ff25159752478477fe42.js
+++ docs/precache-manifest.2068e194c780ff25159752478477fe42.js
@@ -1,102 +1,102 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "002d6944ea497b767b1c5c880580dfae",
-    "url": "/index.html"
+    "revision": "d7dd8721d9715e976b949f3b94aa1b0e",
+    "url": "./index.html"
   },
   {
-    "revision": "c16039776a1006d82192",
-    "url": "/static/css/main.b7c12f70.chunk.css"
+    "revision": "aab8390671818e3e29e5",
+    "url": "./static/css/main.2dd65a99.chunk.css"
   },
   {
     "revision": "3892f1c08089548ee135",
-    "url": "/static/js/2.8fdf2cad.chunk.js"
+    "url": "./static/js/2.8fdf2cad.chunk.js"
   },
   {
     "revision": "d705cb622423d72c5defbf368ca70dcc",
-    "url": "/static/js/2.8fdf2cad.chunk.js.LICENSE"
+    "url": "./static/js/2.8fdf2cad.chunk.js.LICENSE"
   },
   {
-    "revision": "c16039776a1006d82192",
-    "url": "/static/js/main.151936b8.chunk.js"
+    "revision": "aab8390671818e3e29e5",
+    "url": "./static/js/main.6074fa22.chunk.js"
   },
   {
-    "revision": "351896c2f86f2aecd8ce",
-    "url": "/static/js/runtime-main.1977ceff.js"
+    "revision": "5f7b5101eaad9569572e",
+    "url": "./static/js/runtime-main.76ef3f69.js"
   },
   {
     "revision": "d837b441b4af51fc1e6348b933002cee",
-    "url": "/static/media/Air Jordan 6 Retro.d837b441.png"
+    "url": "./static/media/Air Jordan 6 Retro.d837b441.png"
   },
   {
     "revision": "274b87bfa8942415aa5622b09fcf961d",
-    "url": "/static/media/bed.274b87bf.png"
+    "url": "./static/media/bed.274b87bf.png"
   },
   {
     "revision": "698eba0d3248fbd582ec876ff77e2410",
-    "url": "/static/media/bg01.698eba0d.png"
+    "url": "./static/media/bg01.698eba0d.png"
   },
   {
     "revision": "00a64beade1f73e5695078190d8fa951",
-    "url": "/static/media/bg02.00a64bea.png"
+    "url": "./static/media/bg02.00a64bea.png"
   },
   {
     "revision": "72668d9b6e04cacba98ed0a01952cc04",
-    "url": "/static/media/bg03.72668d9b.png"
+    "url": "./static/media/bg03.72668d9b.png"
   },
   {
     "revision": "3597139eb2c521250546d4b682e45b4e",
-    "url": "/static/media/bg04.3597139e.png"
+    "url": "./static/media/bg04.3597139e.png"
   },
   {
     "revision": "dc3d2101cd3d1baa5c7d437fd7875ced",
-    "url": "/static/media/bigShoe.dc3d2101.png"
+    "url": "./static/media/bigShoe.dc3d2101.png"
   },
   {
     "revision": "8b8cbcebcd1961f34eb92a18aee5dc58",
-    "url": "/static/media/brands.8b8cbceb.png"
+    "url": "./static/media/brands.8b8cbceb.png"
   },
   {
     "revision": "234143321a2eecbf9f7b0d9f4a7cbe96",
-    "url": "/static/media/friend01.23414332.png"
+    "url": "./static/media/friend01.23414332.png"
   },
   {
     "revision": "4d855a024f1313f14758c7bbeb475b45",
-    "url": "/static/media/friend02.4d855a02.png"
+    "url": "./static/media/friend02.4d855a02.png"
   },
   {
     "revision": "f16ae38fbc418f4a259f1ddd20ae44ed",
-    "url": "/static/media/friend03.f16ae38f.png"
+    "url": "./static/media/friend03.f16ae38f.png"
   },
   {
     "revision": "5f9df70b8363837292e0d06db05d0212",
-    "url": "/static/media/p01.5f9df70b.png"
+    "url": "./static/media/p01.5f9df70b.png"
   },
   {
     "revision": "df3dfb4bd022e63e8c490e50f62f13da",
-    "url": "/static/media/p02.df3dfb4b.png"
+    "url": "./static/media/p02.df3dfb4b.png"
   },
   {
     "revision": "85bf530c5e71411543532dbdfed3eaf5",
-    "url": "/static/media/p03.85bf530c.png"
+    "url": "./static/media/p03.85bf530c.png"
   },
   {
     "revision": "5458451c8e545adf75077ecefc072f36",
-    "url": "/static/media/p04.5458451c.png"
+    "url": "./static/media/p04.5458451c.png"
   },
   {
     "revision": "1ad7b4c1d098c84a0fa83e45bcf68975",
-    "url": "/static/media/p05.1ad7b4c1.png"
+    "url": "./static/media/p05.1ad7b4c1.png"
   },
   {
     "revision": "a9cab0d0f03d785aebfe78477f37f902",
-    "url": "/static/media/phone01.a9cab0d0.png"
+    "url": "./static/media/phone01.a9cab0d0.png"
   },
   {
     "revision": "051a0f8286c85db6305a5bd6b48ca736",
-    "url": "/static/media/phone02.051a0f82.png"
+    "url": "./static/media/phone02.051a0f82.png"
   },
   {
     "revision": "05a8bb4fe604d54debcd8ff304e268dd",
-    "url": "/static/media/sofa.05a8bb4f.png"
+    "url": "./static/media/sofa.05a8bb4f.png"
   }
 ]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.33fba53c60e2edf391ff25f128610831.js"
+  "./precache-manifest.2068e194c780ff25159752478477fe42.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
